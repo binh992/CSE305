9bce31d9afb44e0b821159be0c80f2bd365bb408
binh992 <binh.nguyenngoc.cit22@eiu.edu.vn>
2025-05-09 16:40:34 +0700

    Add User and Property classes

---
+++ src/Property.java
@@ -0,0 +1,11 @@
+public class Property {
+    private String propertyID;
+    private String description;
+    private String propertyOwnerId;
+
+    public Property(String propertyID, String description, String propertyOwnerId) {
+        this.propertyID = propertyID;
+        this.description = description;
+        this.propertyOwnerId = propertyOwnerId;
+    }
+}
